4ff93a8fcdea7a4c9c383c2affdb6e64e34828f9
ARTHUR <ar.favereaux@gmail.com>
2021-05-16 09:45:51 +0200

    welcome screen

---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Button, ImageBackground, StyleSheet, Text, View } from 'react-native';
+
+function WelcomeScreen(props) {
+    return (
+    <ImageBackground 
+      style={styles.background}
+      source={require('../assets/background.jpg')}>
+        <View style={styles.button}>
+          <Button 
+            color="sienna"
+            title="Entrer"
+            onPress={() => console.log('Button tapped')}/>
+        </View>
+      </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    alignItems: "center",
+  },
+  button: {
+    width: 100,
+    height: 100,
+    position: "absolute",
+    top: 200
+  },
+})
+
+export default WelcomeScreen;
