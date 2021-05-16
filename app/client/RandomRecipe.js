a66d0e5b3603ea734a29954395238c5335438e9a
ARTHUR <ar.favereaux@gmail.com>
2021-05-16 13:52:48 +0200

    affichage titre et recette

--- app/client/RandomRecipe.js
+++ app/client/RandomRecipe.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, SafeAreaView, Text } from 'react-native';
+import { StyleSheet, SafeAreaView, Text, ScrollView, View } from 'react-native';
 
 class RandomRecipe extends Component {
   state = {
@@ -19,12 +19,43 @@ class RandomRecipe extends Component {
   render() {
     return (
       <SafeAreaView>
-          <Text>
-            {this.state.post.title}
-          </Text>
+        <ScrollView contentInsetAdjustmentBehavior="automatic">
+          <Section title={this.state.post.title}/>
+          <Section title="Recipe" description={this.state.post.summary}>
+          </Section>
+        </ScrollView>
       </SafeAreaView>
     );
   }
 };
+
+const Section = ({description, title}) => {
+  return (
+    <View style={styles.sectionContainer}>
+      <Text style={[styles.sectionTitle]}>{title}</Text>
+      <Text style={[styles.sectionDescription]}>{description}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 40,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    alignContent: "center",
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+})
 
 export default RandomRecipe;
--- app/screens/RecipeScreen.js
+++ app/screens/RecipeScreen.js
@@ -23,13 +23,11 @@ function RecipeScreen({ navigation }) {
 const styles = StyleSheet.create({
   background: {
     flex: 1,
+    justifyContent: "flex-end",
     alignItems: "center",
   },
   button: {
-    width: 100,
-    height: 100,
-    position: "absolute",
-    top: 200
+    width: '100%',
   },
 })
 
