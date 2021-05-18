e0e0c932c0df9197097140c7513987f7091a865b
Arthur Favereaux <ar.favereaux@gmail.com>
2021-05-18 08:09:27 +0000

    un / en trop

--- app/client/getRecipe.js
+++ app/client/getRecipe.js
@@ -7,7 +7,7 @@ function getRecipe(id) {
   
   console.log("https://api.spoonacular.com/recipes/"+{identifiant}.identifiant+"/information?includeNutrition=false&apiKey=5323d8a091244877b5e9332e144d9072/")
 
-  fetch("https://api.spoonacular.com/recipes/"+{identifiant}.identifiant+"/information?includeNutrition=false&apiKey=e3d81105854e4678ae22642d2fa3878d/")
+  fetch("https://api.spoonacular.com/recipes/"+{identifiant}.identifiant+"/information?includeNutrition=false&apiKey=e3d81105854e4678ae22642d2fa3878d")
     .then((response) => {return response.json()})
     .then((result) => {setData(result)});
 
